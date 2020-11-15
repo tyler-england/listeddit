71875bba4fe759693091fa33974f8e854e5c676c
Tyler England <54315363+tyler-england@users.noreply.github.com>
2020-11-15 07:35:15 -0500

    Updated data parse

--- listeddit/APIs/from_reddit.py
+++ listeddit/APIs/from_reddit.py
@@ -1,14 +1,17 @@
 import praw
 
 
-def get_comments_lvl1(url):
+def get_comments(lvl1=True):
     ver = findrev()  # TODO: get rev from README
     if ver is None:
         ver = "???"
     ua = "listeddit v: " + ver + " by /u/StPeteTy, github.com/tyler-england/listeddit/"
     reddit = praw.Reddit("listeddit", user_agent=ua)  # TODO: Trap/prompt auth error
     comments = []
-    # TODO: Get comments from thread
+    if lvl1:
+    # TODO: Get top level comments from thread
+    else:
+    # TODO: Get all comments from thread
     return comments  # list of strings
 
 
--- listeddit/data/data_parse.py
+++ listeddit/data/data_parse.py
@@ -1,5 +1,24 @@
 # imports
 
+def get_songs(comments_raw):
+    songs = []
+    # TODO: figure out which part of each element is a song -> create array
+    return songs
+
+
 def get_movies(comments_raw):
-    # TODO: figure out which part of each element is a movie (old versions vs. remakes?)
-    return
+    movies = []
+    # TODO: figure out which part of each element is a movie - >create array (old versions vs. remakes?)
+    return movies
+
+
+def get_shows(comments_raw):
+    shows = []
+    # TODO: figure out which part of each element is a show -> create array
+    return shows
+
+
+def get_games(comments_raw):
+    games = []
+    # TODO: figure out which part of each element is a game -> create array
+    return games
